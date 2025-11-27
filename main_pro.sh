56967c496f93f6064bb0c83987935355d3a34ba5
JP OFFICIAL <hayabusag97@gmail.com>
2025-11-28 01:23:03 +0700

    Add JPVPN installer script
    This script installs necessary packages and downloads the JPVPN official installer.

--- main_pro.sh
+++ main_pro.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+clear
+echo "JPVPN OFFICIAL V3 Installer"
+
+apt update -y && apt upgrade -y
+
+apt install -y wget curl unzip jq
+
+wget -q https://raw.githubusercontent.com/Jpstore1/vip/main/menu.zip -O menu.zip
+unzip -o menu.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/*
+
+echo "Install selesai."
